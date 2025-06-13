2b88699dab67ddd73591c04a34dfd167b0f6dc6c
mehedi <mehedi2hasan7@gmail.com>
2025-06-13 23:52:53 +0600

    added schema

---
+++ lib/db/schema.ts
@@ -0,0 +1,50 @@
+import 'dotenv/config';
+import {pgTable,text,uuid,integer,boolean,timestamp} from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm"
+import { files } from './schema';
+
+
+export const files = pgTable("files",{
+    id:uuid("id").defaultRandom().primaryKey(),
+
+    //basic files and folder information
+
+    name: text("name").notNull(),
+    path: text('path').notNull(), // /document/project/resume
+    size: integer("size").notNull(),
+    type: text("type").notNull(),
+
+    //storage information
+    fileUrl: text("file_url").notNull(),  //url to access
+    thumbnailUrl: text("thumbnail_url"),
+
+    //ownership url 
+    userId: text("user_id").notNull(),
+    parentId: text('parent_id'), //parent folder id (null for root item)
+
+    //file/folder flags
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarted: boolean("is_stared").default(false).notNull(),
+    isTrash: boolean("is_trash").default(false).notNull(),
+
+
+    //timestamped
+    createdAt :timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+
+})
+
+export const filesRelations = relations(files, ({one,many}) =>(
+        {
+            //parent  eeach folder can have many child files/folder
+            parent:one(files, {
+                fields: [files.parentId],
+                references: [files.id]
+            }),
+
+
+            //relationship to child file/folder
+            children: many(files)
+        }
+    ) 
+)
